f9f9208673deddd9667b6662039f9ff2698d999c
laijingfeng <337292616@qq.com>
2017-12-29 21:05:54 +0800

    update

--- jump_helper/jump_helper.py
+++ jump_helper/jump_helper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # encoding=utf-8
-# version: 2017-12-20 16:43:36
+# version: 2017-12-29 21:03:22
 
 import time
 import sys
@@ -16,6 +16,7 @@ import pythoncom
 
 start_pos = (0, 0)
 end_pos = (0, 0)
+config = 390
 
 
 # cmd win on the top
@@ -26,43 +27,48 @@ def topmost_me():
 
 
 def on_keyboard_event(event):
+    if event.Key == "Q":
+        sys.exit()
+    return True
+
+
+def on_mouse_event(event):
     global start_pos
     global end_pos
 
-    if event.Key == "A":
+    if event.MessageName == 'mouse right down':
         start_pos = autopy.mouse.get_pos()
-    elif event.Key == "S":
+    elif event.MessageName == 'mouse right up':
         end_pos = autopy.mouse.get_pos()
         one_step()
-    elif event.Key == "Q":
-        sys.exit()
-
     return True
 
 
 def run():
     topmost_me()
-
     hm = pyHook.HookManager()
     hm.KeyDown = on_keyboard_event
     hm.HookKeyboard()
+    hm.MouseAll = on_mouse_event
+    hm.HookMouse()
     pythoncom.PumpMessages()
 
 
 def one_step():
     global start_pos
     global end_pos
+    global config
 
     x = math.fabs(start_pos[0] - end_pos[0])
     y = math.fabs(start_pos[1] - end_pos[1])
-    z = math.sqrt(x * x + y * y) / 415.0
-    # autopy.mouse.move(322, 602)
+    z = math.sqrt(x * x + y * y) / config
     autopy.mouse.toggle(True)
     time.sleep(z)
     autopy.mouse.toggle(False)
 
-    start_pos = end_pos
-
 
 if __name__ == "__main__":
+    with open('config.txt', 'r') as f:
+        read_data = f.readlines()
+        config = float(read_data[0])
     run()
