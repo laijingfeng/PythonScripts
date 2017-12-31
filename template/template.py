d46b30d376d351a067bc209195ac0ea1d6e68229
laijingfeng <337292616@qq.com>
2017-12-31 17:53:25 +0800

    update

--- template/template.py
+++ template/template.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # encoding=utf-8
-# version: 2017-10-30 11:31:05
+# version: 2017-12-31 17:52:35
 
 import sys
 import os
+import json
 
 enter_cwd_dir = ''
 python_file_dir = ''
-
+test_name = ''
+test_age = ''
 
 def parse_arg(argv):
     if len(argv) < 1:
@@ -34,9 +36,10 @@ if __name__ == '__main__':
 
     print 'start'
     print sys.argv[0], os.getcwd()
-    os.chdir('D:/ADSafe')
-    print sys.argv[0], os.getcwd()
-    with open(get_exe_path('test.txt'), 'r') as f:
-        read_data = f.readlines()
-        for line in read_data:
-            print line
+
+    with open(get_exe_path('./config.json'), 'r') as f:
+        config = json.load(f)
+        test_name = config['test_name']
+        test_age = config['test_age']
+
+    print test_name, test_age
