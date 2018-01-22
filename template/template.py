10fe96908cbfd76de0dbc2b1d89ae8932ee30342
laijingfeng <337292616@qq.com>
2018-01-22 14:51:00 +0800

    整理

--- template/template.py
+++ template/template.py
@@ -1,45 +1,46 @@
+"""code template"""
 #!/usr/bin/python
 # encoding=utf-8
-# version: 2017-12-31 17:52:35
+# version: 2018-01-22 14:48:44
 
 import sys
 import os
 import json
 
-enter_cwd_dir = ''
-python_file_dir = ''
-test_name = ''
-test_age = ''
-
-def parse_arg(argv):
-    if len(argv) < 1:
-        return False, None
-    return True, argv
-
-
-def get_exe_path(simple_path):
-    global enter_cwd_dir
-    global python_file_dir
-    return os.path.join(enter_cwd_dir, python_file_dir, simple_path)
-
-
-def usage():
-    print 'this is usage()'
-
-if __name__ == '__main__':
-    success, args = parse_arg(sys.argv)
-    if not success:
-        usage()
-        exit(-1)
-    enter_cwd_dir = os.getcwd()
-    python_file_dir = os.path.dirname(sys.argv[0])
-
-    print 'start'
-    print sys.argv[0], os.getcwd()
-
-    with open(get_exe_path('./config.json'), 'r') as f:
-        config = json.load(f)
+class MainClass(object):
+    """main class"""
+    def __init__(self):
+        """init the class"""
+        self.enter_cwd_dir = ''
+        self.python_file_dir = ''
+    def parse_arg(self):
+        """parse input args"""
+        if len(sys.argv) < 1:
+            return False, None
+        return True, sys.argv
+    def get_exe_path(self, simple_path):
+        """get the direct path"""
+        return os.path.join(self.enter_cwd_dir, self.python_file_dir, simple_path)
+    def usage(self):
+        """just the usage description of this code"""
+        print 'this is usage()'
+    def run(self):
+        """enter the class"""
+        success, args = self.parse_arg()
+        if not success:
+            self.usage()
+            exit(-1)
+        self.enter_cwd_dir = os.getcwd()
+        self.python_file_dir = os.path.dirname(sys.argv[0])
+        self.work()
+    def work(self):
+        """do real work"""
+        with open(self.get_exe_path('./config.json'), 'r') as read_file:
+            config = json.load(read_file)
         test_name = config['test_name']
         test_age = config['test_age']
+        print test_name, test_age
 
-    print test_name, test_age
+if __name__ == '__main__':
+    MAIN_CLASS = MainClass()
+    MAIN_CLASS.run()
