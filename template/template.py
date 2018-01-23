113c6c31a4f97aa767ee1348dc99546193ac0b12
laijingfeng <337292616@qq.com>
2018-01-23 20:38:13 +0800

    update

--- template/template.py
+++ template/template.py
@@ -1,11 +1,14 @@
-"""code template"""
 #!/usr/bin/python
-# encoding=utf-8
+#-*-coding:utf-8-*-
 # version: 2018-01-22 14:48:44
+"""code template"""
 
 import sys
 import os
 import json
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
 class MainClass(object):
     """main class"""
@@ -33,6 +36,24 @@ class MainClass(object):
         self.enter_cwd_dir = os.getcwd()
         self.python_file_dir = os.path.dirname(sys.argv[0])
         self.work()
+    def send_mail(self, msg):
+        """send mail"""
+        mail_host = 'smtp.exmail.qq.com'
+        mail_user = 'laijf@myjooy.com'
+        mail_pass = 'Lai123'
+        receiver = 'laijf@myjooy.com'
+        message = MIMEText(msg, 'plain', 'utf-8')
+        message['From'] = formataddr(["Jerrylai", mail_user])
+        message['To'] = formataddr(['Jerrylai', receiver])
+        message['Subject'] = '《开心斗舞》音乐配置表'
+        try:
+            smtp_object = smtplib.SMTP_SSL(mail_host, 465)
+            smtp_object.login(mail_user, mail_pass)
+            smtp_object.sendmail(mail_user, [receiver,], message.as_string())
+            smtp_object.quit()
+            return True
+        except smtplib.SMTPException:
+            return False
     def work(self):
         """do real work"""
         with open(self.get_exe_path('./config.json'), 'r') as read_file:
@@ -40,6 +61,7 @@ class MainClass(object):
         test_name = config['test_name']
         test_age = config['test_age']
         print test_name, test_age
+        self.send_mail('你好')
 
 if __name__ == '__main__':
     MAIN_CLASS = MainClass()
