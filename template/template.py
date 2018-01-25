6f28631076b7f5934af9863b364d3ccaec140074
laijingfeng <337292616@qq.com>
2018-01-25 11:08:34 +0800

    规范化

--- template/template.py
+++ template/template.py
@@ -1,53 +1,72 @@
 #!/usr/bin/python
-#-*-coding:utf-8-*-
-# version: 2018-01-22 14:48:44
-"""code template"""
+# encoding=utf-8
+# version: 2018-01-25 10:56:44
+"""
+代码模板
+"""
 
 import sys
 import os
 import json
-import hashlib
 
 class MainClass(object):
-    """main class"""
+    """
+    主类
+    """
     def __init__(self):
-        """init the class"""
+        """
+        初始化
+        """
         self.enter_cwd_dir = ''
         self.python_file_dir = ''
     def parse_arg(self):
-        """parse input args"""
+        """
+        解析参数\n
+        返回是否成和参数列表
+        """
         if len(sys.argv) < 1:
             return False, None
+        # 解析文件名参数
+        # if len(sys.argv) == 1:
+        #     file_name = sys.argv[0]
+        #     file_name = os.path.split(file_name)[1]
+        #     file_name = file_name.split('.')[0]
+        #     pars = file_name.split('^')
+        #     if len(pars) > 1:
+        #         print len(pars), pars[1]
         return True, sys.argv
     def get_exe_path(self, simple_path):
-        """get the direct path"""
+        """
+        相对路径转绝对路径
+        """
         return os.path.join(self.enter_cwd_dir, self.python_file_dir, simple_path)
     def usage(self):
-        """just the usage description of this code"""
+        """
+        使用说明，参数不对的时候会提示
+        """
         print 'this is usage()'
     def run(self):
-        """enter the class"""
+        """
+        类入口
+        """
         success, args = self.parse_arg()
         if not success:
             self.usage()
             exit(-1)
+        if args is None:
+            pass  # 这句只是为了去掉提示变量未使用
         self.enter_cwd_dir = os.getcwd()
         self.python_file_dir = os.path.dirname(sys.argv[0])
         self.work()
-    def cal_md5(self, file_path):
-        """cal md5"""
-        md5_val = ''
-        with open(file_path, 'rb') as f:
-            md5_val = hashlib.md5(f.read()).hexdigest()
-        return md5_val
     def work(self):
-        """do real work"""
+        """
+        do real work
+        """
         with open(self.get_exe_path('./config.json'), 'r') as read_file:
             config = json.load(read_file)
         test_name = config['test_name']
         test_age = config['test_age']
         print test_name, test_age
-        print self.cal_md5('./config.json')
 
 if __name__ == '__main__':
     MAIN_CLASS = MainClass()
